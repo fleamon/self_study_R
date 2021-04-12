8838539d0099da715a4f9345fdb1d2915b58a6bc
wooneo.won <fleamon@nate.com>
2021-04-12 20:03:16 +0900

    r_xlsx_sample.R

---
+++ r_xlsx_sample.R
@@ -0,0 +1,25 @@
+# install.packages("readxl") # readxl package installation
+library(readxl) # install check
+sample_file <- read_excel("/Users/wooneo.won/Application/wooneoR/self_study_R/2019_sample.xlsx")
+View(sample_file)
+sample_file
+# 독립변수 : 국가장학금 지급액 비율, 교내장학금 지급액 비율
+if(F)"
+# 종속변수
+-- 학생에 대한 투자
+재학생1인당 교육비
+재학생1인당 장학금
+재학생1인당 자료구입비
+재학생100명당 강좌수
+-- 교수에 대한 투자
+전임교원 1인당 학생수
+전임교원확보율
+전임교원 강의담당비율
+전임교원 1인당 교내외 연구비
+-- 시설(인프라)에 대한 투자
+교지확보율
+교사시설 확보율
+기술사 수용률
+학생1인당 도서자료수
+"
+sample_file
